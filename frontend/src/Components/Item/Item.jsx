e2a11dfeb3a856ceacbeeb696644373c4d16fa4e
Khánh Duy <duyntk3020@gmail.com>
2024-04-02 17:24:19 +0700

    Item

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import './Item.scss'
 
-export const Item = () => {
+export const Item = (props) => {
   return (
     <div className="item">
-        
+        <img src={props.image} alt="" />
+        <p>{props.name}</p>
+        <div className="item-price">
+            <div className="item-price-new">
+                {props.new_priece}
+            </div>
+            <div className="item-price-old">
+                {props.old_priece}
+            </div>
+        </div>
     </div>
   )
 }
