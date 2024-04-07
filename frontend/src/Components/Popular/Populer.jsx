da2603b2436421512b7824a028772de7e4636b24
Khánh Duy <duyntk3020@gmail.com>
2024-04-07 23:58:30 +0700

    update

---
+++ frontend/src/Components/Popular/Populer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Popular.scss'
+import data_product from '../Assets/data'
+
+export const Populer = () => {
+  return (
+        <div className="popular">
+            h1
+        </div>
+    )
+}
